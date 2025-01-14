679eef77a97350c229ca74100e5959058ed2084a
Teja <teja.rachakonda123@gmail.com>
2025-01-14 08:06:53 +0530

    Time: 10 ms (57.81%) | Memory: 17.9 MB (19.53%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -1,23 +1,17 @@
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
         
-        seen_A = set()  # Tracks elements seen so far in A
-        seen_B = set()  # Tracks elements seen so far in B
+        A_values = set()
+        B_values = set()
+        count = 0
         ans = []
-        common_count = 0  # Tracks the count of common elements
-        
         for i in range(len(A)):
-            # If A[i] is in seen_B, it means it's a common element
-            if A[i] in seen_B:
-                common_count += 1
-            seen_A.add(A[i])  # Add A[i] to seen_A
-            
-            # If B[i] is in seen_A, it means it's a common element
-            if B[i] in seen_A:
-                common_count += 1
-            seen_B.add(B[i])  # Add B[i] to seen_B
-            
-            # Append the current common count to the result
-            ans.append(common_count)
-        
+            if A[i] in B_values:
+                count += 1
+            A_values.add(A[i])
+            if B[i] in A_values:
+                count += 1
+            B_values.add(B[i])
+            ans.append(count)
         return ans
+            
