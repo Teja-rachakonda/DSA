d54eb390c2d0b4e312fa43b1e1901bfd9ea50222
Teja <teja.rachakonda123@gmail.com>
2025-11-16 06:34:08 +0530

    Time: 10 ms (84.27%) | Memory: 18.2 MB (40.82%) - LeetSync

---
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numSub(self, s: str) -> int:
+        MOD = 1_000_000_007
+        total_count = 0
+        current_length = 0
+        for char in s:
+            if char == "1":
+                current_length += 1
+            else:
+                if current_length > 0:
+                    substring_for_block = (current_length * (current_length + 1)) // 2
+                    total_count = (total_count + substring_for_block) % MOD
+
+                current_length = 0
+        
+        if current_length > 0:
+            substring_for_block = (current_length * (current_length + 1)) // 2
+            total_count = (total_count + substring_for_block) % MOD
+        
+        return total_count
+
