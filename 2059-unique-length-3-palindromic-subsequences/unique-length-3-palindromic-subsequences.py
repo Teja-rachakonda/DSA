d0ea8e7475d55c48097b3b7b0430bbfee113c988
Teja <teja.rachakonda123@gmail.com>
2025-11-21 06:19:32 +0530

    Time: 1491 ms (24.80%) | Memory: 18.1 MB (82.86%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -3,12 +3,10 @@ class Solution:
         res = set()
         left = set()
         right = Counter(s)
-
         for m in s:
             right[m] -= 1
             for c in left:
                 if right[c] > 0:
                     res.add((m, c))
             left.add(m)
-        
         return len(res)
