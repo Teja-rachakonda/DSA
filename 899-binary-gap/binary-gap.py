64fe0fb18cd3ccb387f5eb138fc5c205ed5a1477
Teja <teja.rachakonda123@gmail.com>
2026-02-22 23:17:31 +0530

    Time: 4 ms (2.84%) | Memory: 19.5 MB (9.94%) - LeetSync

---
+++ 899-binary-gap/binary-gap.py
@@ -0,0 +1,22 @@
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        max_gap = 0
+        last_pos = -1
+        current_pos = 0
+        
+        
+        while n > 0:
+     
+            if n & 1 == 1:
+            
+                if last_pos != -1:
+                    max_gap = max(max_gap, current_pos - last_pos)
+                
+    
+                last_pos = current_pos
+            
+      
+            n >>= 1
+            current_pos += 1
+            
+        return max_gap
